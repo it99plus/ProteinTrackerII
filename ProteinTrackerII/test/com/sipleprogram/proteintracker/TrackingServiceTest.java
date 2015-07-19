4f0b37e983781f88730fabb769f44c6b8555c411
J Karkar <jkarkar@gmail.com>
2015-07-19 12:56:42 +0100

    Mudul 2 Video 10 - setup/teardown @BeforeClass @AfterClass

--- ProteinTrackerII/test/com/sipleprogram/proteintracker/TrackingServiceTest.java
+++ ProteinTrackerII/test/com/sipleprogram/proteintracker/TrackingServiceTest.java
@@ -2,12 +2,25 @@ package com.sipleprogram.proteintracker;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TrackingServiceTest {
 
-	private TrackingService service; 
+	private TrackingService service;
+
+	@BeforeClass
+	public static void Before() {
+		System.out.println("Before Class");
+	}
+
+	@AfterClass
+	public static void After() {
+		System.out.println("After Class");
+	}
 	
 	@Before
 	public void setUp() {
@@ -15,23 +28,32 @@ public class TrackingServiceTest {
 		service = new TrackingService();
 	}
 
+	@After
+	public void tearDown() {
+		// @After also know as TeaDown - It is useful to clear and rmove
+		// fixtures such as databases
+		// or any files left behind or setup data left behind
+		System.out.println("After");
+	}
+
 	@Test
 	public void NewTrackingServiceTotalSizeIsZero() {
-		assertEquals("Tracking service total was not zero", 0,service.getTotal());
+		assertEquals("Tracking service total was not zero", 0,
+				service.getTotal());
 	}
-	
+
 	@Test
 	public void whenAddingProteinTotalIncreasesByThatAmount() {
 		service.addProtein(10);
-		assertEquals("Proteing amount was not correct", 10,service.getTotal());
-		
+		assertEquals("Proteing amount was not correct", 10, service.getTotal());
+
 	}
-	
+
 	@Test
 	public void whenARemovingProteinTotalRemainsZero() {
 		service.removeProtein(5);
-		assertEquals(0,service.getTotal());
-		
+		assertEquals(0, service.getTotal());
+
 	}
 
 }
