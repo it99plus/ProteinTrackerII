5a746405957f6e0eba0b01c7a295b7e187ee3c95
J Karkar <jkarkar@gmail.com>
2015-07-19 12:47:57 +0100

    Moudule 2 video 8 - @Before

--- ProteinTrackerII/test/com/sipleprogram/proteintracker/TrackingServiceTest.java
+++ ProteinTrackerII/test/com/sipleprogram/proteintracker/TrackingServiceTest.java
@@ -1,14 +1,37 @@
 package com.sipleprogram.proteintracker;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TrackingServiceTest {
 
+	private TrackingService service; 
+	
+	@Before
+	public void setUp() {
+		System.out.println("Before");
+		service = new TrackingService();
+	}
+
 	@Test
 	public void NewTrackingServiceTotalSizeIsZero() {
-		TrackingService service = new TrackingService();
-		assertEquals("Tracking service total was not zero", 1,service.getTotal());
+		assertEquals("Tracking service total was not zero", 0,service.getTotal());
+	}
+	
+	@Test
+	public void whenAddingProteinTotalIncreasesByThatAmount() {
+		service.addProtein(10);
+		assertEquals("Proteing amount was not correct", 10,service.getTotal());
+		
+	}
+	
+	@Test
+	public void whenARemovingProteinTotalRemainsZero() {
+		service.removeProtein(5);
+		assertEquals(0,service.getTotal());
+		
 	}
 
 }
